fb18c207a607daac66a9755ff9c4e840ff379df2
bloesch <bloesch@glue.ch>
2015-05-27 11:05:44 +0200

    Added AGPL v3 license header to java classes.

--- AndroidGLUESigner/src/androidGLUESigner/pdf/ui/PDFViewerFragment.java
+++ AndroidGLUESigner/src/androidGLUESigner/pdf/ui/PDFViewerFragment.java
@@ -1,6 +1,29 @@
 /**
  * 
  */
+/*
+* This program is free software; you can redistribute it and/or modify it under the terms of the 
+* GNU Affero General Public License version 3 as published by the Free Software Foundation 
+* with the addition of the following permission added to Section 15 as permitted in Section 7(a): 
+* FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY Glue Software Engineering AG, 
+* Glue Software Engineering AG DISCLAIMS THE WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+* 
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+* See the GNU Affero General Public License for more details. You should have received a copy 
+* of the GNU Affero General Public License along with this program; if not, 
+* see http://www.gnu.org/licenses or write to the Free Software Foundation, 
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA, 02110-1301 USA.
+
+* The interactive user interfaces in modified source and object code versions of this program 
+* must display Appropriate Legal Notices, as required under Section 5 of the GNU Affero General Public License.
+* 
+* In accordance with Section 7(b) of the GNU Affero General Public License, 
+* a covered work must retain the producer line in every PDF that is created or manipulated using GlueSigner.
+* 
+* For more information, please contact Glue Software Engineering AG at this address: info@glue.ch
+*/
+
 package androidGLUESigner.pdf.ui;
 
 import java.io.File;
@@ -301,12 +324,104 @@ public class PDFViewerFragment extends WizardFragment {
 				try {
 					Intent shareIntent = createShareIntent();
 					 List<Intent> targetedShareIntents = new ArrayList<Intent>();
-					  List<ResolveInfo> resInfo = getActivity().getPackageManager().queryIntentActivities(shareIntent, 0);
+					  List<ResolveInfo> resInfo = getActivity().get/*
+* This program is free software; you can redistribute it and/or modify it under the terms of the 
+* GNU Affero General Public License version 3 as published by the Free Software Foundation 
+* with the addition of the following permission added to Section 15 as permitted in Section 7(a): 
+* FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY Glue Software Engineering AG, 
+* Glue Software Engineering AG DISCLAIMS THE WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+* 
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+* See the GNU Affero General Public License for more details. You should have received a copy 
+* of the GNU Affero General Public License along with this program; if not, 
+* see http://www.gnu.org/licenses or write to the Free Software Foundation, 
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA, 02110-1301 USA.
+
+* The interactive user interfaces in modified source and object code versions of this program 
+* must display Appropriate Legal Notices, as required under Section 5 of the GNU Affero General Public License.
+* 
+* In accordance with Section 7(b) of the GNU Affero General Public License, 
+* a covered work must retain the producer line in every PDF that is created or manipulated using GlueSigner.
+* 
+* For more information, please contact Glue Software Engineering AG at this address: info@glue.ch
+*/
+
+packageManager().queryIntentActivities(shareIntent, 0);
 					  for (ResolveInfo info : resInfo){
 						  Intent targettedShare  = createShareIntent();
 						  // removes GLUESigner from the share list
-						  if (!info.activityInfo.packageName.contains("GLUESigner")){
-							  targettedShare.setPackage(info.activityInfo.packageName);
+						  if (!info.activityInfo./*
+* This program is free software; you can redistribute it and/or modify it under the terms of the 
+* GNU Affero General Public License version 3 as published by the Free Software Foundation 
+* with the addition of the following permission added to Section 15 as permitted in Section 7(a): 
+* FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY Glue Software Engineering AG, 
+* Glue Software Engineering AG DISCLAIMS THE WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+* 
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+* See the GNU Affero General Public License for more details. You should have received a copy 
+* of the GNU Affero General Public License along with this program; if not, 
+* see http://www.gnu.org/licenses or write to the Free Software Foundation, 
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA, 02110-1301 USA.
+
+* The interactive user interfaces in modified source and object code versions of this program 
+* must display Appropriate Legal Notices, as required under Section 5 of the GNU Affero General Public License.
+* 
+* In accordance with Section 7(b) of the GNU Affero General Public License, 
+* a covered work must retain the producer line in every PDF that is created or manipulated using GlueSigner.
+* 
+* For more information, please contact Glue Software Engineering AG at this address: info@glue.ch
+*/
+
+packageName.contains("GLUESigner")){
+							  targettedShare.set/*
+* This program is free software; you can redistribute it and/or modify it under the terms of the 
+* GNU Affero General Public License version 3 as published by the Free Software Foundation 
+* with the addition of the following permission added to Section 15 as permitted in Section 7(a): 
+* FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY Glue Software Engineering AG, 
+* Glue Software Engineering AG DISCLAIMS THE WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+* 
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+* See the GNU Affero General Public License for more details. You should have received a copy 
+* of the GNU Affero General Public License along with this program; if not, 
+* see http://www.gnu.org/licenses or write to the Free Software Foundation, 
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA, 02110-1301 USA.
+
+* The interactive user interfaces in modified source and object code versions of this program 
+* must display Appropriate Legal Notices, as required under Section 5 of the GNU Affero General Public License.
+* 
+* In accordance with Section 7(b) of the GNU Affero General Public License, 
+* a covered work must retain the producer line in every PDF that is created or manipulated using GlueSigner.
+* 
+* For more information, please contact Glue Software Engineering AG at this address: info@glue.ch
+*/
+
+package(info.activityInfo./*
+* This program is free software; you can redistribute it and/or modify it under the terms of the 
+* GNU Affero General Public License version 3 as published by the Free Software Foundation 
+* with the addition of the following permission added to Section 15 as permitted in Section 7(a): 
+* FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY Glue Software Engineering AG, 
+* Glue Software Engineering AG DISCLAIMS THE WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+* 
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+* See the GNU Affero General Public License for more details. You should have received a copy 
+* of the GNU Affero General Public License along with this program; if not, 
+* see http://www.gnu.org/licenses or write to the Free Software Foundation, 
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA, 02110-1301 USA.
+
+* The interactive user interfaces in modified source and object code versions of this program 
+* must display Appropriate Legal Notices, as required under Section 5 of the GNU Affero General Public License.
+* 
+* In accordance with Section 7(b) of the GNU Affero General Public License, 
+* a covered work must retain the producer line in every PDF that is created or manipulated using GlueSigner.
+* 
+* For more information, please contact Glue Software Engineering AG at this address: info@glue.ch
+*/
+
+packageName);
 							  targetedShareIntents.add(targettedShare);
 						  }
 					  }
